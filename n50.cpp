5039609f0974661404a7a935ae03ba1ed25e08ba
Francesca Giordano <fg6@sanger.ac.uk>
2016-10-04 12:41:16 +0100

    added seq on single-line, faster read, total number of bases is long int now

--- n50.cpp
+++ n50.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>    // sort
 #include <numeric> // accumulate
 #include <iomanip>  //setprecision
+#include <fstream>
+#include <sstream>
 
 static gzFile fp;
 static  std::vector<int> rlen;
@@ -14,6 +16,7 @@ static  std::vector<int> rlen;
 KSEQ_INIT(gzFile, gzread)
 int readseqs(void);
 int calc(void);
+int myread(char* file);
 
 int main(int argc, char *argv[])
 { 
@@ -27,7 +30,10 @@ int main(int argc, char *argv[])
     return 1;
   }
   
-  readseqs();
+  int mlines=myread(argv[1]); //does not work with seq on multi-lines! but a lot faster!
+  
+  if(mlines)
+  	readseqs();
   calc();  
 
   return 0;
@@ -37,26 +43,38 @@ int main(int argc, char *argv[])
 int calc()
 // ---------------------------------------- //
 {
+  int pri=0;
 
+  if(pri)std::cout<< "Now calc " << std::endl;
   sort(rlen.begin(),  rlen.end(), std::greater<int>());
 
   int n=rlen.size();
+  if(pri)std::cout << "number of contigs/reads " << n << std::endl;
   int max=rlen[0];                 	
   float bases = accumulate(rlen.begin(), rlen.end(), 0.0);
+  if(pri)std::cout << "bases " << bases <<  std::endl;
   float mean = bases / n;
+  if(pri)std::cout << "mean " << mean <<  std::endl;
 
   int n50=0,l50=0;
   int done=0;
-  int t50=0;
+  long int t50=0;
   int ii=0;
   while(done<1){
+    if(pri)if(ii>3693240) std::cout << ii << std::endl;
     t50+=rlen[ii];
+    if(pri)if(ii>3693240) std::cout << " " << t50 << std::endl;
     if(t50 > bases*0.5) 
       done=1;
+    if(pri)if(ii>3693240) std::cout << " " << done << std::endl;
     ii++;
-   }  
-   n50=ii-1;  //counting from 0
-   l50=rlen[n50]; 
+   }
+  if(pri)std::cout << "t50 " << t50<< " ii " << ii  <<  std::endl;
+
+  n50=ii-1;  //counting from 0
+  l50=rlen[n50]; 
+  if(pri)std::cout << "n50 " << n50 << " l50 " << l50 <<  std::endl;
+
 
   std::cout << std::fixed << std::setprecision(0) <<  "Bases= " << bases << " contigs= "<< n << " mean_length= " 
 	<< mean << " longest= " << max << " l50= "<< l50 << " n50= " << n50+1   //counting from 1
@@ -72,7 +90,9 @@ int readseqs(){
   kseq_t *seq;
   int l=-1;
   seq = kseq_init(fp);
-  rlen.reserve(200000);
+  rlen.reserve(300000);
+
+  if(0)std::cout << " ...using kseq to read file" << std::endl;
 
   while ((l = kseq_read(seq)) >= 0) 
     rlen.push_back(seq->seq.l);
@@ -80,6 +100,50 @@ int readseqs(){
   kseq_destroy(seq);
   gzclose(fp);
   
+ return 0;
+}
+
+// ---------------------------------------- //
+int myread(char* file)   //FILE *namef)
+// ---------------------------------------- //
+{ // won't work with seq on multilines!
+  char fq[5]={"@"};
+  char fa[5]={">"};
+  char plus[5]={"+"};
+  int readevery=1;
+  std::ifstream infile(file);
+  rlen.reserve(300000);
+
+
+  std::string line; 
+  getline(infile,line);
+  if(line.at(0)==fq[0]) readevery=4;  // fastq input file
+  else if(line.at(0)==fa[0]) readevery=2;  // fasta input file
+  else std::cout << " Error: cannot determine if input file is fasta or fastq " << std::endl;
+
+  getline(infile,line);
+  getline(infile,line);
+
+
+  if(line.at(0)!=plus[0]){ 
+	if(0)std::cout<< "Sequences on multiple lines..." << std::endl;  // seq on single line 
+	return(1);
+  }else{
+        if(0)std::cout<< "Sequences on single line " << std::endl;
+  }
+
+  int nseq=-1;
+  int readnext=1;
+  infile.clear();  // start over
+  infile.seekg (0, std::ios::beg);
+  while (getline(infile,line))
+   {
+     nseq++;
+     if(nseq==readnext){
+      rlen.push_back(line.length());
+      readnext+=readevery;
+     }
+   }
  return 0;
 }
 
