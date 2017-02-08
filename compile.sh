acdc861b297fe50d32e9cce4373193374b6234a3
Francesca Giordano <fg6@sanger.ac.uk>
2017-02-08 10:26:26 +0000

    check if gzstream is available

--- compile.sh
+++ compile.sh
@@ -1,11 +1,17 @@
 mygzstream=~/software/GZSTREAM/gzstream
 
 check=`echo ${CPLUS_INCLUDE_PATH} | grep gzstream`
-if [[ -n ${check} ]]; then
-	echo gzstream found
-else
-        echo gzstream not found!
-	export CPLUS_INCLUDE_PATH=$mygzstream:${CPLUS_INCLUDE_PATH}
+
+if [[ ! -n ${check} ]]; then
+	if [[ -n ${mygzstream} ]]; then
+		export CPLUS_INCLUDE_PATH=$mygzstream:${CPLUS_INCLUDE_PATH}
+	else
+		echo 
+	        echo " Error!! gzstream not found in your CPLUS_INCLUDE_PATH"
+		echo " Please add gzstream to your CPLUS_INCLUDE_PATH"
+		echo "  or include the path in the mygzstream variable on line 1 of this script ($0) then 'source $0'" 
+		echo
+	fi
 fi
 make clean all
 
